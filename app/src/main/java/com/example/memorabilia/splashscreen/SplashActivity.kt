cfd0798749a75d8f18bd978fd3695f7d23516521
leond <leondrahrf@gmail.com>
2024-05-29 23:39:24 +0700

    add userdao

--- app/src/main/java/com/example/memorabilia/splashscreen/SplashActivity.kt
+++ app/src/main/java/com/example/memorabilia/splashscreen/SplashActivity.kt
@@ -1,21 +1,36 @@
 package com.example.memorabilia.splashscreen
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.bumptech.glide.Glide
 import com.example.memorabilia.R
+import com.example.memorabilia.databinding.ActivitySplashBinding
+import com.example.memorabilia.main.MainActivity
 
-class SplashActivity : AppCompatActivity() {
+class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_splash)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        val splashScreenActivity = installSplashScreen()
+        setContentView(binding.root)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 2000)
+
+        // Load the GIF using Glide
+        Glide.with(this)
+            .asGif()
+            .load(R.drawable.splashlogo) // Replace with your GIF file name
+            .into(binding.imageView)
     }
 }
